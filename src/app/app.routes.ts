dc31da2c1b72ab55bd9ca7d3907042bb8696b24a
siwar-zr <siwarzrelli180959@gmail.com>
2026-02-23 13:58:06 +0100

    Template Driven / Reactive Forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { NgClassComponent } from './components/ng-class/ng-class.component';
+import { TemplateFormComponent } from './components/forms/template-form/template-form.component';
+import { ReactiveFormComponent } from './components/forms/reactive-form/reactive-form.component';
 
 export const routes: Routes = [
     {
@@ -20,6 +22,14 @@ export const routes: Routes = [
     {
         path:'ng-class',
         component: NgClassComponent
+    },
+    {
+        path:'template-form',
+        component: TemplateFormComponent
+    },
+    {
+        path:'reactive-form',
+        component: ReactiveFormComponent
     }
 
 ];
